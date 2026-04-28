fbd8d318fd9955c20b1bec40d616799b3577aa7d
Choi Sang Hyeok <cch130223@gmail.com>
2026-04-28 12:53:52 +0900

    scanf값을 출력할때 주소지정문자인 &를 빼먹어서 추가

--- Multiplication.c
+++ Multiplication.c
@@ -3,9 +3,9 @@ int main(void)
 {
   int num1, num2;
   printf("Enter the first number : \n");
-  scanf("%d\n", num1);
+  scanf("%d", &num1);
   printf("Enter the second number : \n");
-  scanf("%d\n", num2);
-  printf("Multiplication value is : %d\n", num1 * num2);
+  scanf("%d", &num2);
+  printf("Multiplication value is %d\n", num1 * num2);
   return 0;
 }
