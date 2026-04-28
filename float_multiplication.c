7cd0c0b362578ed8515d09cf50425044f2a6f757
Choi Sang Hyeok <cch130223@gmail.com>
2026-04-28 13:06:57 +0900

    실수출력지정자를 이용하여 두 실수의 곱셈값을 구하는것
    %f가 실수출력지정자이고 ".2"는 소숫점 두번쨰 자리까지 출력하라는 의미이다

--- float_multiplication.c
+++ float_multiplication.c
@@ -3,9 +3,9 @@ int main(void)
 {
     float num1, num2;
     printf("Enter the first float value : \n");
-    scanf("%.2f", num1);
+    scanf("%.2f", &num1);
     printf("Enter the second float value : \n");
-    scanf("%.2f", num2);
+    scanf("%.2f", &num2);
     printf("Multiplication of two float value is %.2f\n", num1 * num2);
     return 0;
 }
