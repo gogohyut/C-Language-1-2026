a0193e0f859d4d04333bcd708b8943de56144ea3
Choi Sang Hyeok <cch130223@gmail.com>
2026-05-12 20:18:28 +0900

    do-while 반복문을 사용해 1부터 n까지 홀수의 합을 구하는 코드

---
+++ do-while_odd_number_calculate.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(void)
+{
+  int i = 1;
+  int n;
+  int sum = 0;
+  scanf("%d", &n);
+  
+    do 
+    {
+        if (i % 2 != 0) 
+        {
+            sum += i;
+        }
+        i++;
+    } while (i <= n);
+
+    printf("%d\n", n, sum);
+    return 0;
+}
