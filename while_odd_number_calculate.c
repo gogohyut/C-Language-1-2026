bbc2bdcd3e834768c4ea45491747a7ef93b6fe59
Choi Sang Hyeok <cch130223@gmail.com>
2026-05-12 16:39:48 +0900

    while반복문을 사용해 1부터 n까지 홀수의 합을 구하는 코드

---
+++ while_odd_number_calculate.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(void)
+{
+  int i = 1;
+  int n;
+  int sum = 0;
+  scanf("%d", &sum);
+  while(i <= n);
+  {
+    if(i %2 != 0);
+      sum += i;
+    i++;
+  }
+  printf("%d", sum);
+  return 0;
+}
