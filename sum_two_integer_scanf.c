91fe61ad37b8b7a5e3996c67f2c2d67ed1dc054d
Choi Sang Hyeok <cch130223@gmail.com>
2026-05-19 18:15:30 +0900

    두 개의 입력 정수를 이용하여 합계를 구하는 코드

---
+++ sum_two_integer_scanf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void)
+{
+    int i, n;
+    int sum = 0;
+    scanf("%d", &i);
+    scanf("%d", &n);
+    sum = i + n;
+    printf("%d + %d = %d\n", i, n, i + n);
+    return 0;
+
+}
