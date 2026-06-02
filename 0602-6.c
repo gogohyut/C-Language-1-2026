dd0f7716cf862ac132ac9ad8ad38f497cf3c931c
Choi Sang Hyeok <cch130223@gmail.com>
2026-06-02 19:23:34 +0900

    for 문을 사용하여 3x3 배열에 1~9 숫자를 순서대로 채운 후, 각 행의 숫자들과 그 행의 합계를 함께 출력하는 예제 코드

---
+++ 0602-6.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int arr[3][3];
+    int val = 1;
+    int sum = 0;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            arr[i][j] = val++;
+            printf("%d ", arr[i][j]);
+            sum += arr[i][j];
+        }
+        printf("%d\n", sum);
+        sum = 0;
+    }
+    return 0;
+}
